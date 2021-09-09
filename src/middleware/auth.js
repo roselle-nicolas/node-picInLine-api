2e742f5ef0e665f3dfd8565a4ec9d4e3f9e10c1c
nicolas roselle <60002258+roselle-nicolas@users.noreply.github.com>
2021-09-09 17:21:30 +0200

    code review

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,19 +8,20 @@ module.exports = (req, res, next) => {
     return res.status(401).json({ message })
   }
    
-    const token = authorizationHeader.split(' ')[1]
-    jwt.verify(token, process.env.RANDOM_TOKEN_SECRET, (error, decodedToken) => {
-        if(error) {
-            const message = `The user is not authorized to access this data.`
-            return res.status(401).json({ message, data: error })
-        }
-    
-        const userId = decodedToken.userId
-        if (req.body.userId && req.body.userId !== userId) {
-            const message = `User ID is invalid.`
-            res.status(401).json({ message })
-        } else {
-            next()
-        }
-    })
+  const token = authorizationHeader.split(' ')[1]
+  
+  jwt.verify(token, process.env.RANDOM_TOKEN_SECRET, (error, decodedToken) => {
+      if(error) {
+          const message = `The user is not authorized to access this data.`
+          return res.status(401).json({ message, data: error })
+      }
+
+      const userId = decodedToken.userId
+      if (req.body.userId && req.body.userId !== userId) {
+          const message = `User ID is invalid.`
+          res.status(401).json({ message })
+      } else {
+          next()
+      }
+  })
 }
